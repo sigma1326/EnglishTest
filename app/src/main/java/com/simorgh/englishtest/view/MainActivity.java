7aeac652fff19c1af80e83e5c11f7d8a825049a2
sigma <taleb.zarhesh@gmail.com>
2019-01-11 12:58:24 +0330

    add drawer to app

--- app/src/main/java/com/simorgh/englishtest/view/MainActivity.java
+++ app/src/main/java/com/simorgh/englishtest/view/MainActivity.java
@@ -2,8 +2,10 @@ package com.simorgh.englishtest.view;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
+import com.google.android.material.navigation.NavigationView;
 import com.simorgh.database.TestRepository;
 import com.simorgh.englishtest.R;
 import com.simorgh.englishtest.adapter.OuterAdapter;
@@ -14,10 +16,16 @@ import com.simorgh.garlandview.header.HeaderTransformer;
 
 import java.util.Objects;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.core.view.ViewCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import io.github.inflationx.viewpump.ViewPumpContextWrapper;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     TailRecyclerView rv;
 
     @Override
@@ -28,6 +36,23 @@ public class MainActivity extends AppCompatActivity {
         View rootView = findViewById(android.R.id.content);
         rootView.setBackgroundDrawable(getResources().getDrawable(R.drawable.background));
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle("نام برنامه");
+
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ViewCompat.setLayoutDirection(drawer, ViewCompat.LAYOUT_DIRECTION_RTL);
+        ViewCompat.setLayoutDirection(navigationView, ViewCompat.LAYOUT_DIRECTION_LTR);
 
 
         rv = findViewById(R.id.rv_main);
@@ -43,6 +68,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
     protected void onStart() {
         super.onStart();
     }
@@ -52,4 +87,8 @@ public class MainActivity extends AppCompatActivity {
         super.attachBaseContext(ViewPumpContextWrapper.wrap(newBase));
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
 }
